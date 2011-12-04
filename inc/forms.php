d16e527e387c68db770241db75a679c84b3191e1
George Stephanis <George@Stephanis.info>
2011-12-03 21:55:15 -0500

    Added very rudimentary form function -- and backward compatibility to php 5.2 and earlier with json_encode json_decode and get_called_class

--- inc/forms.php
+++ inc/forms.php
@@ -1,2 +1,14 @@
 <?php
 
+function _form_row( $name, $label, $value = NULL, $type = 'text', $options = array() ){
+	$rand = rand(10000,99999);
+	if( 'text' == $type ):
+		?>	<tr id="form-row-<?php echo $name; ?>" class="form-row form-row-<?php echo $type; ?>">
+				<th scope="row"><label for="frm_<?php echo $name; ?>"><?php echo $label; ?></label></th>
+				<td><input type="text" id="frm_<?php echo $name; ?>" name="<?php echo $name; ?>" value="<?php echo $value; ?>" /></td>
+			</tr>
+		<?php
+	# More to come soon ...
+	endif;
+	return TRUE;
+}
